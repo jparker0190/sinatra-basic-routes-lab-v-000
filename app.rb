29a61a0acc0ce939e444d1f2be7ab22d4958d893
Joseph Parker <jparker010190@gmail.com>
2018-05-20 22:25:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class App < Sinatra::Base
   get '/name' do
     "My name is ___"
   end
+  get '/name' do
+    "My name is ___"
+  end
+  get '/favorite-song' do
+    "My favorite song is ___"
+  end
 end
